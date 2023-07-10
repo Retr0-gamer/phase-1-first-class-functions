2fa5c1692c7a636c4f2490ac5dda2f16fa7e6220
Retr0-gamer <salimhetham@gmail.com>
2023-07-10 14:29:47 +0300

    completed

---
+++ index.js
@@ -0,0 +1,13 @@
+function receivesAFunction (callback) {
+  callback () 
+}
+
+function returnsANamedFunction (named) {
+  return function named () {
+
+  }
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {}  
+}
